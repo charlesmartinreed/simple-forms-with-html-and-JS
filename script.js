a73d18888912c68ae9c621163d7ec0895fe83e84
charlesmartinreed <charlesmartinreed@icloud.com>
2019-12-02 13:49:33 -0600

    Add validateEmail function.

--- script.js
+++ script.js
@@ -31,6 +31,9 @@ const animatedForm = () => {
         //   move on to the next thing
         console.log("all clear");
         moveToNextSlide(parent, nextForm);
+      } else if (input.type === "email" && validateEmail(input)) {
+        console.log("all clear");
+        moveToNextSlide(parent, nextForm);
       }
     });
   });
@@ -49,10 +52,27 @@ const validateUser = user => {
   }
 };
 
+const validateEmail = email => {
+  // courtesy of the one and only Tyler McGinnis
+  let result = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value);
+
+  if (result) {
+    console.log("valid email");
+    document.body.style.backgroundImage = `linear-gradient(165deg, ${gradientColorsSuccess[0]}, ${gradientColorsSuccess[1]}`;
+
+    return true;
+  } else {
+    console.log("invalid email");
+    document.body.style.backgroundImage = `linear-gradient(165deg, ${gradientColorsDanger[0]}, ${gradientColorsDanger[1]}`;
+
+    return false;
+  }
+};
+
 const moveToNextSlide = (parent, nextForm) => {
-  setInterval(() => {
-    document.body.style.backgroundImage = `linear-gradient(165deg, ${gradientColorsNeutral[0]}, ${gradientColorsNeutral[1]}`;
-  }, 3000);
+  // setInterval(() => {
+  //   document.body.style.backgroundImage = `linear-gradient(165deg, ${gradientColorsNeutral[0]}, ${gradientColorsNeutral[1]}`;
+  // }, 1000);
 
   parent.classList.add("inactive");
   parent.classList.remove("active");
